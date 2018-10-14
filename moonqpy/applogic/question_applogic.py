9241cdbe111d0f759d5c339b1f914f4129bef066
Gatsby Lee <thewayofthinking@gmail.com>
2018-10-14 00:04:26 -0700

    add list_questions with dummy data

---
+++ moonqpy/applogic/question_applogic.py
@@ -0,0 +1,22 @@
+"""
+@author Gatsby Lee
+@since 2018-10-13
+"""
+
+
+class QuestionApplogic(object):
+    def get_questions(self):
+        return (
+            (1, 'hello world'),
+            (2, 'hello world2'),
+            (3, 'hello world3'),
+        )
+
+    def add_question(self, qobj):
+        pass
+
+    def rm_question(self, qobj):
+        pass
+
+    def update_question(self, qobj):
+        pass
--- moonquestion/views/question_view.py
+++ moonquestion/views/question_view.py
@@ -1,11 +1,16 @@
 """
+@author Gatsby Lee
+@since 2018-10-14
+
 View for Questions
 """
-from pyramid.response import Response
+
 from pyramid.view import view_config
 
+from moonqpy.applogic import QuestionApplogic
 
-@view_config(route_name='list_questions')
+
+@view_config(route_name='list_questions', renderer='json')
 def list_questions(request):
-    print('Incoming request')
-    return Response('<body><h1>Hello World!</h1></body>')
+    qlist = QuestionApplogic().get_questions()
+    return {'qlist': qlist}
